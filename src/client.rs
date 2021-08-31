156797816afe431574e2f26376a5b6d5f4c04d8f
Alexei Zarubni <alex.zarubni@gmail.com>
2021-08-31 16:36:36 +0300

    add corrections after review

--- src/client.rs
+++ src/client.rs
@@ -1,54 +1,57 @@
-use jsonrpc_core_client::transports::local;
 use jsonrpc_core::{Error, IoHandler, Result};
+use jsonrpc_core_client::transports::local;
 use jsonrpc_derive::rpc;
 
 /// Accumulate API trait
 #[rpc]
 pub trait AccumulateRPC {
-  /// Returns a protocol version
-  #[rpc(name = "protocolVersion")]
-  fn protocol_version(&self) -> Result<String>;
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
 
-  /// accepts URL and returns a result as ADI object
-  #[rpc(name = "adi", alias("callAsyncGetAccumulateIdentity"))]
-  fn getADI(&self, u: &str) -> Result<ADI>;
+	/// accepts URL and returns a result as ADI object
+	#[rpc(name = "adi", alias("callAsyncGetAccumulateIdentity"))]
+	fn getADI(&self, u: &str) -> Result<ADI>;
 
-   // accepts ADI structure with url and hash, signer, time and sginature
-  ///  and returns a result as created ADI object
-  #[rpc(name = "adi-create", alias("callAsyncCreateAccumulateIdentity"))]
-  fn createADI(&self, u:ADI, s:Signer, t:UnixTime, sig: &str ) -> Result<ADI>;
+	// accepts ADI structure with url and hash, signer, time and sginature
+	///  and returns a result as created ADI object
+	#[rpc(name = "adi-create", alias("callAsyncCreateAccumulateIdentity"))]
+	fn createADI(&self, u: ADI, s: Signer, t: UnixTime, sig: &str) -> Result<ADI>;
 
-  ///... other RPC methods go here
+	///... other RPC methods go here
 
-  /// Performs asynchronous operation
-  #[rpc(name = "callAsync")]
-  fn call(&self, u: string) -> FutureResult<String, Error>;
+	/// Performs asynchronous operation
+	#[rpc(name = "callAsync")]
+	fn call(&self, u: string) -> FutureResult<String, Error>;
 }
 
 struct AccumulateRPCImpl;
 
 impl AccumulateRPC for AccumulateRPCImpl {
-  fn protocol_version(&self) -> Result<String> {
-      Ok("version1".into())
-  }
+	fn protocol_version(&self) -> Result<String> {
+		Ok("version1".into())
+	}
 
-  fn getADI(&self, a: string) -> Result<ADI> {
-      Ok(a)
-  }
+	fn getADI(&self, a: string) -> Result<ADI> {
+		Ok(a)
+	}
 
-  fn call(&self, _: u64) -> FutureResult<String, Error> {
-      future::ok("OK".to_owned())
-  }
+	fn call(&self, _: u64) -> FutureResult<String, Error> {
+		future::ok("OK".to_owned())
+	}
 }
 
 fn main() {
-  let mut io = IoHandler::new();
-  io.extend_with(RpcImpl.to_delegate());
+	let mut io = IoHandler::new();
+	io.extend_with(RpcImpl.to_delegate());
 
-  let fut = {
-      let (client, server) = local::connect::<gen_client::Client, _, _>(io);
-      // Make an actual call to API
-      client.getADI("some-unique-url").map(|res| println!("adi = {}", res)).join(server)
-  };
-  fut.wait().unwrap();
+	let fut = {
+		let (client, server) = local::connect::<gen_client::Client, _, _>(io);
+		// Make an actual call to API
+		client
+			.getADI("some-unique-url")
+			.map(|res| println!("adi = {}", res))
+			.join(server)
+	};
+	fut.wait().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,67 @@
-
 #[macro_use]
 extern crate jsonrpc_client_core;
 
+extern crate derive_builder;
 extern crate serde;
 extern crate serde_json;
-extern crate derive_builder;
 
-use serde::{Serialize, Deserialize};
 use derive_builder::Builder;
+use serde::{Deserialize, Serialize};
+
 /// ADIURL
 ///
 /// Accumulate ADI URL
 ///
 pub type ADIURL = String;
+
 /// ADIPublicKeyHash
 ///
 /// SHA-256 hash of ADI Public Key
 ///
 pub type ADIPublicKeyHash = String;
+
 /// ADIPublicKey
 ///
 /// ADI Public Key in hex format
 ///
 pub type ADIPublicKey = String;
+
 /// TokenURL
 ///
 /// Token URL
 ///
 pub type TokenURL = String;
+
 /// TokenSymbol
 ///
 /// Token ticker
 ///
 pub type TokenSymbol = String;
+
 /// TokenPrecision
 ///
 /// All transactions will need to specify input/output amounts in integer units × 10^precision
 ///
 pub type TokenPrecision = i64;
+
 /// TokenAccountURL
 ///
 /// Token Account URL
 ///
 pub type TokenAccountURL = String;
+
 /// SenderTokenAccount
 ///
 /// Token Account URL
 ///
 pub type SenderTokenAccount = String;
+
 /// Amount
 ///
 /// Number of tokens
 ///
 pub type Amount = i64;
+
 /// ReceiverTokenAccount
 ///
 /// Token Account URL and amount
@@ -61,26 +70,30 @@ pub type Amount = i64;
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct ReceiverTokenAccount {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<TokenAccountURL>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub amount: Option<Amount>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<TokenAccountURL>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub amount: Option<Amount>,
 }
+
 /// ReceiverSTokenAccountS
 ///
 /// Token Account URL
 ///
 pub type ReceiverSTokenAccountS = Vec<ReceiverTokenAccount>;
+
 /// Metadata
 ///
 /// Transaction Metadata
 ///
 pub type Metadata = String;
+
 /// Balance
 ///
 /// Token Account balance
 ///
 pub type Balance = i64;
+
 /// Hash
 ///
 /// Transaction Hash
@@ -91,20 +104,23 @@ pub type StringXtH3HHU8 = String;
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct ADIAccumulateDigitalIdentity {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<ADIURL>,
-    #[serde(rename = "publicKeyHash", skip_serializing_if = "Option::is_none")]
-    pub public_key_hash: Option<ADIPublicKeyHash>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<ADIURL>,
+	#[serde(rename = "publicKeyHash", skip_serializing_if = "Option::is_none")]
+	pub public_key_hash: Option<ADIPublicKeyHash>,
 }
+
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct Signer {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<ADIURL>,
-    #[serde(rename = "publicKey", skip_serializing_if = "Option::is_none")]
-    pub public_key: Option<ADIPublicKey>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<ADIURL>,
+	#[serde(rename = "publicKey", skip_serializing_if = "Option::is_none")]
+	pub public_key: Option<ADIPublicKey>,
 }
+
 pub type Integer2AHOqbcQ = i64;
 pub type String7GgoCKrm = String;
 pub type StringWcSuVYcM = String;
@@ -112,57 +128,62 @@ pub type StringWcSuVYcM = String;
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct Token {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<TokenURL>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub symbol: Option<TokenSymbol>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub precision: Option<TokenPrecision>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<TokenURL>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub symbol: Option<TokenSymbol>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub precision: Option<TokenPrecision>,
 }
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct TokenAccount {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<TokenAccountURL>,
-    #[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
-    pub token_url: Option<TokenURL>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<TokenAccountURL>,
+	#[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
+	pub token_url: Option<TokenURL>,
 }
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct TokenTx {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub from: Option<SenderTokenAccount>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub to: Option<ReceiverSTokenAccountS>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub meta: Option<Metadata>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub from: Option<SenderTokenAccount>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub to: Option<ReceiverSTokenAccountS>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub meta: Option<Metadata>,
 }
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
 #[builder(setter(strip_option), default)]
 #[serde(default)]
 pub struct TokenAccountWithBalance {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub url: Option<TokenAccountURL>,
-    #[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
-    pub token_url: Option<TokenURL>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub balance: Option<Balance>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub url: Option<TokenAccountURL>,
+	#[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
+	pub token_url: Option<TokenURL>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub balance: Option<Balance>,
 }
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(untagged)]
-pub enum AnyOfStringXtH3HHU8ADIAccumulateDigitalIdentitySignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenSignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenAccountSignerInteger2AHOqbcQString7GgoCKrmString7GgoCKrmTokenTxSignerInteger2AHOqbcQString7GgoCKrmADIAccumulateDigitalIdentityADIAccumulateDigitalIdentityTokenTokenTokenAccountWithBalanceTokenAccountTokenTxTokenTx {
-    StringXtH3HHU8(StringXtH3HHU8),
-    ADIAccumulateDigitalIdentity(ADIAccumulateDigitalIdentity),
-    Signer(Signer),
-    Integer2AHOqbcQ(Integer2AHOqbcQ),
-    String7GgoCKrm(String7GgoCKrm),
-    StringWcSuVYcM(StringWcSuVYcM),
-    Token(Token),
-    TokenAccount(TokenAccount),
-    TokenTx(TokenTx),
-    TokenAccountWithBalance(TokenAccountWithBalance),
+pub enum AnyOfStringXtH3HHU8ADIAccumulateDigitalIdentitySignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenSignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenAccountSignerInteger2AHOqbcQString7GgoCKrmString7GgoCKrmTokenTxSignerInteger2AHOqbcQString7GgoCKrmADIAccumulateDigitalIdentityADIAccumulateDigitalIdentityTokenTokenTokenAccountWithBalanceTokenAccountTokenTxTokenTx
+{
+	StringXtH3HHU8(StringXtH3HHU8),
+	ADIAccumulateDigitalIdentity(ADIAccumulateDigitalIdentity),
+	Signer(Signer),
+	Integer2AHOqbcQ(Integer2AHOqbcQ),
+	String7GgoCKrm(String7GgoCKrm),
+	StringWcSuVYcM(StringWcSuVYcM),
+	Token(Token),
+	TokenAccount(TokenAccount),
+	TokenTx(TokenTx),
+	TokenAccountWithBalance(TokenAccountWithBalance),
 }
 
 jsonrpc_client!(pub struct AccumulateAPI {
