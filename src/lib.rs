9fdfb9099ceca51733001b40e5c292ad269eddd3
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2023-03-02 10:55:05 +0300

    (feat) upgrade client interface; use more ergonomic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,198 +1,91 @@
-#[macro_use]
-extern crate jsonrpc_client_core;
 
-extern crate derive_builder;
-extern crate serde;
-extern crate serde_json;
 
-use derive_builder::Builder;
-use serde::{Deserialize, Serialize};
+use std::{fmt, sync::Arc};
+//use async_trait::async_trait;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::time::Duration;
+use std::time::SystemTime;
 
-/// ADIURL
-///
-/// Accumulate ADI URL
-///
-pub type ADIURL = String;
+pub mod error;
+pub use error::{Error};
 
-/// ADIPublicKeyHash
-///
-/// SHA-256 hash of ADI Public Key
-///
-pub type ADIPublicKeyHash = String;
+////////////////////////////////////////////////////////////////////////////////
+// USEFUL DEFAULTS
 
-/// ADIPublicKey
-///
-/// ADI Public Key in hex format
-///
-pub type ADIPublicKey = String;
+pub const ACCUMULATE_MAINNET_RPC_URL: &str = "https://mainnet.accumulatenetwork.io";
+pub const ACCUMULATE_TESTNET_KERMIT_RPC_URL: &str = "https://api-gateway.accumulate.defidevs.io";
+pub const ACCUMULATE_TESTNET_FOZZIE_RPC_URL: &str = "https://fozzie.accumulatenetwork.io";
+pub const ACCUMULATE_EDGE_RPC_URL: &str = "https://api.accumulate.chainfold.io";
+pub const ACCUMULATE_DEFAULT_RPC_URL: &str = "http://127.0.0.1:4467"; // if none speficied, use devnet
 
-/// TokenURL
-///
-/// Token URL
-///
-pub type TokenURL = String;
+/// The default timeout for API requests
+pub const DEFAULT_TIMEOUT: u64 = 120;
 
-/// TokenSymbol
-///
-/// Token ticker
-///
-pub type TokenSymbol = String;
+/// JSON RPC version
+pub const JSON_RPC: &str = "2.0";
 
-/// TokenPrecision
-///
-/// All transactions will need to specify input/output amounts in integer units × 10^precision
-///
-pub type TokenPrecision = i64;
+////////////////////////////////////////////////////////////////////////////////
+// Client Logic
 
-/// TokenAccountURL
-///
-/// Token Account URL
-///
-pub type TokenAccountURL = String;
+// Response type
+pub type Result<T = ()> = std::result::Result<T, Error>;
 
-/// SenderTokenAccount
-///
-/// Token Account URL
-///
-pub type SenderTokenAccount = String;
-
-/// Amount
-///
-/// Number of tokens
-///
-pub type Amount = i64;
-
-/// ReceiverTokenAccount
-///
-/// Token Account URL and amount
-///
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct ReceiverTokenAccount {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<TokenAccountURL>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub amount: Option<Amount>,
+#[derive(Clone, Debug)]
+pub struct Client {
+    base_url: String,
+    client: reqwest::Client,
 }
 
-/// ReceiverSTokenAccountS
-///
-/// Token Account URL
-///
-pub type ReceiverSTokenAccountS = Vec<ReceiverTokenAccount>;
-
-/// Metadata
-///
-/// Transaction Metadata
-///
-pub type Metadata = String;
-
-/// Balance
-///
-/// Token Account balance
-///
-pub type Balance = i64;
-
-/// Hash
-///
-/// Transaction Hash
-///
-pub type Hash = String;
-pub type StringXtH3HHU8 = String;
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct ADIAccumulateDigitalIdentity {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<ADIURL>,
-	#[serde(rename = "publicKeyHash", skip_serializing_if = "Option::is_none")]
-	pub public_key_hash: Option<ADIPublicKeyHash>,
+impl Default for Client {
+    fn default() -> Self {
+        Self::new_with_base_url(ACCUMULATE_DEFAULT_RPC_URL.to_string())
+    }
 }
 
+impl Client {
+    /// Create a new client using a given base URL and a default
+    /// timeout. The library will use absoluate paths based on this
+    /// base_url.
+    pub fn new_with_base_url(base_url: String) -> Self {
+        Self::new_with_timeout(base_url, DEFAULT_TIMEOUT)
+    }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct Signer {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<ADIURL>,
-	#[serde(rename = "publicKey", skip_serializing_if = "Option::is_none")]
-	pub public_key: Option<ADIPublicKey>,
+    /// Create a new client using a given base URL, and request
+    /// timeout value.  The library will use absoluate paths based on
+    /// the given base_url.
+    pub fn new_with_timeout(base_url: String, timeout: u64) -> Self {
+        let client = reqwest::Client::builder()
+            .gzip(true)
+            .timeout(Duration::from_secs(timeout))
+            .build()
+            .unwrap();
+        Self { base_url, client }
+    }
+
+    async fn post<T: DeserializeOwned, D: Serialize>(&self, path: &str, data: D) -> Result<T> {
+        #[derive(Clone, Serialize, Deserialize, Debug)]
+        #[serde(untagged)]
+        pub(crate) enum Response<T> {
+            Data { result: T, id: String },
+            Error { id: String, error: Error },
+        }
+
+        #[derive(Clone, Serialize, Deserialize, Debug)]
+        pub(crate) struct Error {
+            message: String,
+            code: isize,
+        }
+
+        let request_url = format!("{}{}", self.base_url, path);
+        let request = self.client.post(&request_url).json(&data);
+        let response = request.send().await?;
+        let body = response.text().await?;
+        let v: Response<T> = serde_json::from_str(&body)?;
+        match v {
+            Response::Data { result, .. } => Ok(result),
+            Response::Error { error, .. } => {
+                Err(error::Error::NodeError(error.message, error.code))
+            }
+        }
+    }
 }
-
-pub type Integer2AHOqbcQ = i64;
-pub type String7GgoCKrm = String;
-pub type StringWcSuVYcM = String;
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct Token {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<TokenURL>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub symbol: Option<TokenSymbol>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub precision: Option<TokenPrecision>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct TokenAccount {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<TokenAccountURL>,
-	#[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
-	pub token_url: Option<TokenURL>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct TokenTx {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub from: Option<SenderTokenAccount>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub to: Option<ReceiverSTokenAccountS>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub meta: Option<Metadata>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Builder, Default)]
-#[builder(setter(strip_option), default)]
-#[serde(default)]
-pub struct TokenAccountWithBalance {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub url: Option<TokenAccountURL>,
-	#[serde(rename = "tokenURL", skip_serializing_if = "Option::is_none")]
-	pub token_url: Option<TokenURL>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub balance: Option<Balance>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-#[serde(untagged)]
-pub enum AnyOfStringXtH3HHU8ADIAccumulateDigitalIdentitySignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenSignerInteger2AHOqbcQString7GgoCKrmStringWcSuVYcMTokenAccountSignerInteger2AHOqbcQString7GgoCKrmString7GgoCKrmTokenTxSignerInteger2AHOqbcQString7GgoCKrmADIAccumulateDigitalIdentityADIAccumulateDigitalIdentityTokenTokenTokenAccountWithBalanceTokenAccountTokenTxTokenTx
-{
-	StringXtH3HHU8(StringXtH3HHU8),
-	ADIAccumulateDigitalIdentity(ADIAccumulateDigitalIdentity),
-	Signer(Signer),
-	Integer2AHOqbcQ(Integer2AHOqbcQ),
-	String7GgoCKrm(String7GgoCKrm),
-	StringWcSuVYcM(StringWcSuVYcM),
-	Token(Token),
-	TokenAccount(TokenAccount),
-	TokenTx(TokenTx),
-	TokenAccountWithBalance(TokenAccountWithBalance),
-}
-
-jsonrpc_client!(pub struct AccumulateAPI {
-  pub fn Adi(&mut self, url: StringXtH3HHU8) -> RpcRequest<ADIAccumulateDigitalIdentity>;
-pub fn AdiCreate(&mut self, adi: ADIAccumulateDigitalIdentity, signer: Signer, timestamp: Integer2AHOqbcQ, sig: String7GgoCKrm) -> RpcRequest<ADIAccumulateDigitalIdentity>;
-pub fn Token(&mut self, url: StringWcSuVYcM) -> RpcRequest<Token>;
-pub fn TokenCreate(&mut self, token: Token, signer: Signer, timestamp: Integer2AHOqbcQ, sig: String7GgoCKrm) -> RpcRequest<Token>;
-pub fn TokenAccount(&mut self, url: StringWcSuVYcM) -> RpcRequest<TokenAccountWithBalance>;
-pub fn TokenAccountCreate(&mut self, tokenAccount: TokenAccount, signer: Signer, timestamp: Integer2AHOqbcQ, sig: String7GgoCKrm) -> RpcRequest<TokenAccount>;
-pub fn TokenTx(&mut self, hash: String7GgoCKrm) -> RpcRequest<TokenTx>;
-pub fn TokenTxCreate(&mut self, tokenTx: TokenTx, signer: Signer, timestamp: Integer2AHOqbcQ, sig: String7GgoCKrm) -> RpcRequest<TokenTx>;
-});
