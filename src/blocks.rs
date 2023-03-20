a79c18513c1fce1c71b485e55a95762e994ab72b
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2023-03-20 21:12:44 +0300

    (fix) correct blocks retrieval functions

---
+++ src/blocks.rs
@@ -0,0 +1,21 @@
+use crate::*;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+/// Represents a block response from blockchain-node.
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct BlockRaw {
+    pub height: u64,
+    pub hash: String,
+    pub prev_hash: String,
+    pub time: u64,
+    //pub transactions: Vec<BlockTransaction>,
+}
+
+pub async fn get_range(client: &Client, scope: &str, start: u64, count: u64, expand: bool) -> Result<Vec<BlockRaw>> {
+    let json = json!(ApiCall::query_major_blocks(scope.to_string(), start, count, expand));
+    println!("{}", json);
+
+    let url_path = &("/v".to_string() + client.version.to_string().as_str());
+    client.post(url_path, &json).await
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use std::{fmt, sync::Arc};
 use async_trait::async_trait;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::time::Duration;
 use std::time::SystemTime;
+use std::{fmt, sync::Arc};
 
 pub mod error;
-pub use error::{Error};
+pub use error::Error;
 
 pub mod utils;
-pub use utils::{now_millis};
+pub use utils::now_millis;
+
+pub mod blocks;
 
 ////////////////////////////////////////////////////////////////////////////////
 // USEFUL DEFAULTS
@@ -36,166 +38,223 @@ pub type Result<T = ()> = std::result::Result<T, Error>;
 
 #[derive(Clone, Debug)]
 pub struct Client {
-    base_url: String,
-    version: u8,
-    client: reqwest::Client,
+	base_url: String,
+	version: u8,
+	client: reqwest::Client,
 }
 
 impl Default for Client {
-    fn default() -> Self {
-        Self::new_with_base_url(ACCUMULATE_DEFAULT_RPC_URL.to_string())
-    }
+	fn default() -> Self {
+		Self::new_with_base_url(ACCUMULATE_DEFAULT_RPC_URL.to_string())
+	}
 }
 
 impl Client {
+	pub fn new() -> Self {
+		let client = reqwest::Client::builder()
+			//.gzip(true)
+			.timeout(Duration::from_secs(DEFAULT_TIMEOUT))
+			.build()
+			.unwrap();
+		Self {
+			base_url: ACCUMULATE_DEFAULT_RPC_URL.to_string(),
+			version: ACCUMULATE_API_VER,
+			client,
+		}
+	}
 
-    pub fn new() -> Self {
-	let client = reqwest::Client::builder()
-            .gzip(true)
-            .timeout(Duration::from_secs(DEFAULT_TIMEOUT))
-            .build()
-            .unwrap();
-	Self {base_url: ACCUMULATE_DEFAULT_RPC_URL.to_string(), version: ACCUMULATE_API_VER, client}
-    }
+	/// Create a new client using a given base URL and a default
+	/// timeout. The library will use absoluate paths based on this
+	/// base_url.
+	pub fn new_with_base_url(base_url: String) -> Self {
+		Self::new_with_timeout(base_url, ACCUMULATE_API_VER, DEFAULT_TIMEOUT)
+	}
 
-    /// Create a new client using a given base URL and a default
-    /// timeout. The library will use absoluate paths based on this
-    /// base_url.
-    pub fn new_with_base_url(base_url: String) -> Self {
-        Self::new_with_timeout(base_url, ACCUMULATE_API_VER, DEFAULT_TIMEOUT)
-    }
+	/// Create a new client using a given base URL, and request
+	/// timeout value.  The library will use absoluate paths based on
+	/// the given base_url.
+	pub fn new_with_timeout(base_url: String, version: u8, timeout: u64) -> Self {
+		let client = reqwest::Client::builder()
+			//.gzip(true)
+			.timeout(Duration::from_secs(timeout))
+			.build()
+			.unwrap();
+		Self {
+			base_url,
+			version,
+			client,
+		}
+	}
 
-    /// Create a new client using a given base URL, and request
-    /// timeout value.  The library will use absoluate paths based on
-    /// the given base_url.
-    pub fn new_with_timeout(base_url: String, version: u8, timeout: u64) -> Self {
-        let client = reqwest::Client::builder()
-            .gzip(true)
-            .timeout(Duration::from_secs(timeout))
-            .build()
-            .unwrap();
-        Self { base_url, version, client }
-    }
+	pub fn new_with_version(base_url: String, version: u8) -> Self {
+		let client = reqwest::Client::builder()
+			//.gzip(true)
+			.timeout(Duration::from_secs(DEFAULT_TIMEOUT))
+			.build()
+			.unwrap();
+		Self {
+			base_url,
+			version,
+			client,
+		}
+	}
 
-    pub fn new_with_version(base_url: String, version: u8) -> Self {
-	 let client = reqwest::Client::builder()
-            .gzip(true)
-            .timeout(Duration::from_secs( DEFAULT_TIMEOUT))
-            .build()
-            .unwrap();
-	 Self { base_url, version, client }
-    }
+	async fn post<T: DeserializeOwned, D: Serialize>(&self, path: &str, data: D) -> Result<T> {
+		#[derive(Clone, Serialize, Deserialize, Debug)]
+		#[serde(untagged)]
+		pub(crate) enum Response<T> {
+			Data { result: T, id: String },
+			Error { id: String, error: Error },
+		}
 
-    async fn post<T: DeserializeOwned, D: Serialize>(&self, path: &str, data: D) -> Result<T> {
-        #[derive(Clone, Serialize, Deserialize, Debug)]
-        #[serde(untagged)]
-        pub(crate) enum Response<T> {
-            Data { result: T, id: String },
-            Error { id: String, error: Error },
-        }
+		#[derive(Clone, Serialize, Deserialize, Debug)]
+		pub(crate) struct Error {
+			message: String,
+			code: isize,
+		}
 
-        #[derive(Clone, Serialize, Deserialize, Debug)]
-        pub(crate) struct Error {
-            message: String,
-            code: isize,
-        }
+		let request_url = format!("{}{}", self.base_url, path);
+		println!("1// request URL: {}", request_url);
+		let request = self.client.post(&request_url).json(&data);
+		let requestV = self.client.post(&request_url).json(&data);
+		println!("2//");
 
-        let request_url = format!("{}{}", self.base_url, path);
-        let request = self.client.post(&request_url).json(&data);
-        let response = request.send().await?;
-        let body = response.text().await?;
-        let v: Response<T> = serde_json::from_str(&body)?;
-        match v {
-            Response::Data { result, .. } => Ok(result),
-            Response::Error { error, .. } => {
-                Err(error::Error::NodeError(error.message, error.code))
-            }
-        }
-    }
+		match requestV.build() {
+			Ok(req) => {
+				// Inspect the request
+				println!("{:?}", req);
+				//println!("{:?}", &data);
+
+				// Send the request
+				//let response = client.execute(req).await?;
+				// Handle the response...
+			}
+			Err(e) => {
+				// Handle the error
+				eprintln!("Error building the request: {:?}", e);
+			}
+		}
+
+		let response = request.send().await?;
+		println!("3//");
+		let body = response.text().await?;
+		println!("body: {}", body);
+		let v: Response<T> = serde_json::from_str(&body)?;
+		match v {
+			Response::Data { result, .. } => Ok(result),
+			Response::Error { error, .. } => Err(error::Error::ApiError(error.message, error.code)),
+		}
+	}
 }
 
 #[derive(Clone, Deserialize, Debug, Serialize)]
 #[serde(tag = "method")]
-#[serde(rename_all = "snake_case")]
+//#[serde(rename_all = "snake_case")]
 enum Method {
-    Metrics,
-    ConsensusStatus,
-    FindService,
-    MajorBLocksGet {params: BlockListParams},
-    MinorBlocksGet {params: BlockListParams},
-    MajorBlockGet {params: BlockParams},
-    MinorBlockGet {params: BlockParams},
-    Query { params: QueryParams}
+	#[serde(rename(serialize = "metrics"))]
+	Metrics,
+	#[serde(rename(serialize = "consensus-status"))]
+	ConsensusStatus,
+	#[serde(rename(serialize = "find-service"))]
+	FindService,
+	#[serde(rename(serialize = "query"))]
+	MajorBlocksGet { params: QueryBlockListParams },
+	#[serde(rename(serialize = "query"))]
+	MinorBlocksGet { params: BlockListParams },
+	#[serde(rename(serialize = "query"))]
+	MajorBlockGet { params: BlockParams },
+	#[serde(rename(serialize = "query"))]
+	MinorBlockGet { params: BlockParams },
+	#[serde(rename(serialize = "query"))]
+	Query { params: QueryParams },
 }
 
 #[derive(Clone, Deserialize, Debug, Serialize)]
 pub(crate) struct ApiCall {
-    jsonrpc: String,
-    id: String,
-    #[serde(flatten)]
-    method: Method,
+	jsonrpc: String,
+	id: String,
+	#[serde(flatten)]
+	method: Method,
 }
 
-
 impl ApiCall {
-    fn new(request: Method) -> ApiCall {
-        ApiCall {
-            jsonrpc: JSON_RPC.to_string(),
-            id: now_millis(),
-            method: request,
-        }
-    }
+	fn new(request: Method) -> ApiCall {
+		ApiCall {
+			jsonrpc: JSON_RPC.to_string(),
+			id: "0".to_string(), //now_millis(),
+			method: request,
+		}
+	}
 
-   pub(crate) fn query_major_blocks(url: String, start: u64, count: u64, expand: bool) -> Self {
-        Self::new(Method::MajorBLocksGet {
-            params: BlockListParams { scope: url, start, count, expand },
-        })
-   }
+	pub(crate) fn query_major_blocks(url: String, start: u64, count: u64, expand: bool) -> Self {
+		Self::new(Method::MajorBlocksGet {
+			params: QueryBlockListParams {
+				scope: url,
+				query: {
+					BlockListParams {
+						query_type: "block".to_string(),
+						major_range: BlockListRangeParams { start, count, expand },
+					}
+				},
+			},
+		})
+	}
 
-   pub(crate) fn query_major_block(url: String, height: u64) -> Self {
-        Self::new(Method::MajorBlockGet {
-            params: BlockParams { scope: url, height },
-        })
-   }
-
-
+	pub(crate) fn query_major_block(url: String, height: u64) -> Self {
+		Self::new(Method::MajorBlockGet {
+			params: BlockParams { scope: url, height },
+		})
+	}
 }
 
 #[derive(Clone, Deserialize, Debug, Serialize)]
 struct QueryParams {
-    address: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    height: Option<u64>,
+	scope: String,
 }
 
 #[derive(Clone, Deserialize, Debug, Serialize)]
 struct BlockParams {
-    scope: String,
-    height: u64,
+	scope: String,
+	height: u64,
+}
+
+#[derive(Clone, Deserialize, Debug, Serialize)]
+struct QueryBlockListParams {
+	scope: String,
+	query: BlockListParams,
 }
 
 #[derive(Clone, Deserialize, Debug, Serialize)]
 struct BlockListParams {
-    scope: String,
-    start: u64,
-    count: u64,
-    expand: bool
+	#[serde(rename(serialize = "queryType"))]
+	query_type: String,
+	#[serde(rename(serialize = "majorRange"))]
+	major_range: BlockListRangeParams,
 }
 
+#[derive(Clone, Deserialize, Debug, Serialize)]
+struct BlockListRangeParams {
+	start: u64,
+	count: u64,
+	expand: bool,
+}
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use tokio::test;
+	use super::*;
+	use tokio::test;
 
-    #[test]
-    async fn get_last_major_block() {
-        let client = Client::default();
-        let txn = query_major_blocks(&client, "").await;
-        let er = match txn {
-            Err(e) => format!("{}", e),
-            _ => panic!("??"),
-        };
-        //assert_eq!(er, "error code -100 from node: No Blocks available");
-    }
+	#[test]
+	async fn get_last_major_block() {
+		let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
+		let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true).await;
+
+		if let Err(err) = resp {
+			// can be omitted if
+			println!("{}", err);
+			panic!("{err}"); // only the Ok result
+		}
+
+		assert!(1 > 0);
+	}
 }
