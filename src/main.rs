aea94c6721f85f4e9f390310d1e6538cbce0f6c0
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2023-04-05 23:49:26 +0300

    (feat) add more examples with reverse major block retrieval - from last to first

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,33 @@ use types::*;
 #[tokio::main]
 async fn main() {
 	env_logger::init();
-	let ov = get_last_major_block().await;
+	let ov = get_last_major_blocks().await;
 	match ov {
 		None => println!("no result"),
 		Some(v) => println!("{:?}", v),
 	}
 }
 
-async fn get_last_major_block() -> Option<MajorBlocksRangeResponse> {
+async fn get_last_major_blocks() -> Option<MajorBlocksRangeResponse> {
 	let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
-	let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true).await;
+	let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true, true).await;
+
+	match resp {
+		Err(e) => {
+			println!("{}", e);
+			return None;
+		}
+		Ok(r) => return Some(r),
+		Ok(_) | Err(_) => {
+			println!("error");
+			return None;
+		}
+	}
+}
+
+async fn get_first_major_blocks() -> Option<MajorBlocksRangeResponse> {
+	let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
+	let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true, false).await;
 
 	match resp {
 		Err(e) => {
