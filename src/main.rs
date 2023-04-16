4ea463632a1548bdb2f9b98bb12a938e7372882d
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2023-04-16 23:01:44 +0300

    (feat) update work with ranges

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use types::*;
 #[tokio::main]
 async fn main() {
 	env_logger::init();
-	let ov = get_last_major_blocks().await;
+	let ov = get_last_minor_blocks().await;
 	match ov {
 		None => println!("no result"),
 		Some(v) => println!("{:?}", v),
@@ -15,7 +15,7 @@ async fn main() {
 
 async fn get_last_major_blocks() -> Option<MajorBlocksRangeResponse> {
 	let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
-	let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true, true).await;
+	let resp = blocks::get_range_major(&client, "acc://bvn-Apollo.acme", 1, 100, true, true).await;
 
 	match resp {
 		Err(e) => {
@@ -32,7 +32,24 @@ async fn get_last_major_blocks() -> Option<MajorBlocksRangeResponse> {
 
 async fn get_first_major_blocks() -> Option<MajorBlocksRangeResponse> {
 	let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
-	let resp = blocks::get_range(&client, "acc://bvn-Apollo.acme", 1, 100, true, false).await;
+	let resp = blocks::get_range_major(&client, "acc://bvn-Apollo.acme", 1, 100, true, false).await;
+
+	match resp {
+		Err(e) => {
+			println!("{}", e);
+			return None;
+		}
+		Ok(r) => return Some(r),
+		Ok(_) | Err(_) => {
+			println!("error");
+			return None;
+		}
+	}
+}
+
+async fn get_last_minor_blocks() -> Option<MajorBlocksRangeResponse> {
+	let client = Client::new_with_version(ACCUMULATE_MAINNET_RPC_URL.to_string(), 3);
+	let resp = blocks::get_range_minor(&client, "acc://bvn-Apollo.acme", 1, 10, true, true).await;
 
 	match resp {
 		Err(e) => {
